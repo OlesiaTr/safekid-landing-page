77be6a0b1fe6307857a1de21118595b5de9cbaab
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-04-14 22:43:38 +0300

    + links const

---
+++ src/constants/links.ts
@@ -0,0 +1,19 @@
+const LINKS = {
+  home: '/',
+  relevance: '#relevance',
+  description: '#description',
+  functionality: '#functionality',
+  aboutUs: '#about-us',
+  downloadApp: '#download-app',
+  faq: '#faq',
+};
+
+export const {
+  home,
+  aboutUs,
+  relevance,
+  description,
+  functionality,
+  downloadApp,
+  faq,
+} = LINKS;
