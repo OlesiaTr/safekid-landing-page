95ca04fb074579d4011d1f93fffb9c46204f1107
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-04-14 22:31:59 +0300

    + menu-btn ui

---
+++ src/components/ui/menu-btn/menu-btn.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+
+export interface Props {
+  ariaLabel: string;
+  onClick: () => void;
+  isOpen: boolean;
+}
+
+export const MenuBtn: FC<Props> = ({ ariaLabel, onClick, isOpen }) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      aria-label={ariaLabel}
+      className="transition lg:hidden"
+    >
+      {isOpen ? (
+        <XMarkIcon className="size-8" />
+      ) : (
+        <Bars3Icon className="size-8" />
+      )}
+    </button>
+  );
+};
