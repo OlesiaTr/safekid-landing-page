828e4ad1ecfd3f15c2b846213853373ad3dc3f5b
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-04-14 23:15:20 +0300

    + navigation ui

---
+++ src/components/ui/navigation/navigation.tsx
@@ -0,0 +1,30 @@
+import { smoothScroll } from '@/utils';
+import React, { FC, MouseEvent } from 'react';
+
+export interface Props {
+  location: string;
+  links: {
+    path: string;
+    label: string;
+  }[];
+}
+
+export const Navigation: FC<Props> = ({ location, links }) => {
+  return (
+    <nav className={location === 'header' ? 'hidden lg:block' : ''}>
+      <ul className="flex gap-4 xl:gap-12">
+        {links.map((item, id) => (
+          <li key={id}>
+            <a
+              href={item.path}
+              className="flex py-[32px] text-lg font-medium leading-normal xl:text-xl"
+              onClick={(e: MouseEvent) => smoothScroll(e, item.path)}
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
