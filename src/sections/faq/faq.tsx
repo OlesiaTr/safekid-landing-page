2dfc10152a9112790d5b1a7b22cbc202402118c0
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-05-02 16:04:30 +0300

    * update faq section

--- src/sections/faq/faq.tsx
+++ src/sections/faq/faq.tsx
@@ -9,6 +9,9 @@ import clsx from 'clsx';
 
 export const FAQ = () => {
   const [active, setActive] = useState<null | number>(null);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onMoreDetailsClick = () => setIsClicked(!isClicked);
 
   const toggleActive = (id: number) => {
     setActive(active === id ? null : id);
@@ -19,20 +22,20 @@ export const FAQ = () => {
       <Title title={data.title} />
 
       <ul className=" flex flex-col gap-2 md:gap-4 ">
-        {data.questionData.map((item, id) => (
+        {data.questionData.slice(0, 5).map(item => (
           <li
-            key={id}
-            onClick={() => toggleActive(id)}
+            key={item.id}
+            onClick={() => toggleActive(+item.id)}
             className={clsx(
               ' cursor-pointer transition',
-              active === id && 'active',
+              active === +item.id && 'active',
             )}
           >
             <div className="flex items-baseline ">
               <ChevronDownIcon
                 className={clsx(
                   'size-6 min-w-6 transition',
-                  active === id && 'rotate-180',
+                  active === +item.id && 'rotate-180',
                 )}
               />
               <p className="ml-2 text-xl font-semibold leading-normal md:text-2xl xl:text-3xl">
@@ -40,13 +43,59 @@ export const FAQ = () => {
               </p>
             </div>
 
-            {active === id && (
+            {active === +item.id && (
               <p className="ml-8 leading-normal md:text-lg xl:text-2xl">
                 {item.answer}
               </p>
             )}
           </li>
         ))}
+
+        {isClicked ? (
+          <>
+            <span
+              className="cursor-pointer select-none leading-normal text-blue underline transition md:text-lg xl:text-2xl "
+              onClick={onMoreDetailsClick}
+            >
+              {data.inMoreDetails.clickableToClose}
+            </span>
+            {data.questionData.slice(5).map(item => (
+              <li
+                key={item.id}
+                onClick={() => toggleActive(+item.id)}
+                className={clsx(
+                  ' cursor-pointer transition',
+                  active === +item.id && 'active',
+                )}
+              >
+                <div className="flex items-baseline ">
+                  <ChevronDownIcon
+                    className={clsx(
+                      'size-6 min-w-6 transition',
+                      active === +item.id && 'rotate-180',
+                    )}
+                  />
+                  <p className="ml-2 text-xl font-semibold leading-normal md:text-2xl xl:text-3xl">
+                    {item.question}
+                  </p>
+                </div>
+
+                {active === +item.id && (
+                  <p className="ml-8 leading-normal md:text-lg xl:text-2xl">
+                    {item.answer}
+                  </p>
+                )}
+              </li>
+            ))}
+          </>
+        ) : (
+          <span
+            className="cursor-pointer select-none leading-normal text-blue underline transition md:text-lg xl:text-2xl "
+            onClick={onMoreDetailsClick}
+          >
+            {data.inMoreDetails.clickableToOpen}
+          </span>
+        )}
       </ul>
     </section>
   );
