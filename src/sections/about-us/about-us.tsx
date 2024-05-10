f302377798849c58ebac9d9a32d88184d5ca27f4
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2024-05-10 14:34:59 +0300

    * updated about-us info

--- src/sections/about-us/about-us.tsx
+++ src/sections/about-us/about-us.tsx
@@ -1,11 +1,16 @@
-import { YoutubeEmbed } from '@/components/ui';
+import { Title, YoutubeEmbed } from '@/components/ui';
 
 import data from '@/data/about-us.json';
 
 export const AboutUs = () => {
   return (
-    <section className="section" id={data.id}>
-      <div className="w-full ">
+    <section className="section gap-16 md:flex-row" id={data.id}>
+      <div className="  self-start md:flex-1 ">
+        <Title title={data.title} />
+
+        <p className="leading-normal md:text-lg xl:text-2xl ">{data.label}</p>
+      </div>
+      <div className="m-auto md:w-[700px] md:flex-1 ">
         <YoutubeEmbed videoSrc={data.videoSrc} videoTitle={data.videoTitle} />
       </div>
     </section>
